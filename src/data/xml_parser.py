476561e336ac7023c39228cf5de0c57977308ab6
Paweł Rzepiński <rzepinskip@student.mini.pw.edu.pl>
2018-12-22 14:39:19 +0100

    Use generators and parallelism

--- src/data/xml_parser.py
+++ src/data/xml_parser.py
@@ -4,18 +4,19 @@ import os
 from typing import List
 from glob import glob
 from lxml import etree
+import concurrent.futures as cf
 
 logger = logging.getLogger(__name__)
 
 
 def extract_all_book_xml_roots(xmls_dir: str) -> List[etree.Element]:
     logger.info(f"Processing xml files in {xmls_dir} ...")
-    book_roots = list()
-    for filename in glob(os.path.join(xmls_dir, '*.xml')):
-        logger.debug(f"Processing file {filename} ...")
-        book_roots.append(_extract_book_element_(filename))
 
-    return book_roots
+    with cf.ThreadPoolExecutor() as executor:
+        xml_files = glob(os.path.join(xmls_dir, '*.xml'))
+        for xml_file, book_root in zip(
+                xml_files, executor.map(_extract_book_element_, xml_files)):
+            yield book_root
 
 
 def _extract_book_element_(filename: str) -> etree.Element:
