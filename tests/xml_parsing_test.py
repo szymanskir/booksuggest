8188ed29cea8f1e617cc0d8a6546b5e19adec582
Paweł Rzepiński <rzepinskip@student.mini.pw.edu.pl>
2018-12-22 21:19:22 +0100

    Fix xml parsing references in tests

--- tests/xml_parsing_test.py
+++ tests/xml_parsing_test.py
@@ -1,7 +1,8 @@
 import pytest
 import re
 import os
-from src.data.parse_xml_files import _extract_book_element_, _extract_book_info_, _extract_similar_books_
+import src.data.xml_parser
+import src.data.clean_book
 
 current_path = os.path.dirname(os.path.realpath(__file__))
 simple_file_path = os.path.join(current_path, "data/test-simple.xml")
@@ -13,19 +14,19 @@ full_file_path = os.path.join(current_path, "data/test-full.xml")
     full_file_path,
 ])
 def test_book_element_parsed(filename):
-    book = _extract_book_element_(filename)
+    book = src.data.xml_parser._extract_book_element_(filename)
     assert book is not None
 
 
 @pytest.mark.parametrize("filename, expected", [
-    (simple_file_path, ["41335427", "9780439785969",
-                         "The war against Voldemort is not going well"]),
-    (full_file_path, ["908211", "9780441788385",
-                      "<b>NAME: Valentine Michael Smith<br />ANCESTRY: Human<br />ORIGIN: Mars</b><br /><br />Valentine Michael Smith is a human being raised on Mars, newly returned to Earth. Among his people for the first time, he struggles to understand the social mores and prejudices of human nature that are so alien to him, while teaching them his own fundamental beliefs in grokking, watersharing, and love."]),
+    (simple_file_path, (41335427, "9780439785969",
+                         "The war against Voldemort is not going well")),
+    (full_file_path, (908211, "9780441788385",
+                      "<b>NAME: Valentine Michael Smith<br />ANCESTRY: Human<br />ORIGIN: Mars</b><br /><br />Valentine Michael Smith is a human being raised on Mars, newly returned to Earth. Among his people for the first time, he struggles to understand the social mores and prejudices of human nature that are so alien to him, while teaching them his own fundamental beliefs in grokking, watersharing, and love.")),
 ])
 def test_book_info_parsed(filename, expected):
-    book = _extract_book_element_(filename)
-    book_info = _extract_book_info_(book)
+    book = src.data.xml_parser._extract_book_element_(filename)
+    book_info = src.data.clean_book._extract_book_info_(book)
     assert book_info == expected
 
 
