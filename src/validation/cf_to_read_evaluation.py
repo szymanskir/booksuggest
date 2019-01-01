f6249ea9e709918900a95e83916f76ed36e523af
Paweł Rzepiński <rzepinskip@student.mini.pw.edu.pl>
2019-01-01 17:37:34 +0100

    Add thresholded precision and recall to t_read evaluation

--- src/validation/cf_to_read_evaluation.py
+++ src/validation/cf_to_read_evaluation.py
@@ -3,37 +3,50 @@ from os import listdir
 from os.path import join
 import click
 import pandas as pd
+from typing import Tuple
 
-from .metrics import precision
+from .metrics import precision_thresholded, recall_thresholded
 
 
 def evaluate_to_read(
         predictions_df: pd.DataFrame,
-        to_read_df: pd.DataFrame
-) -> float:
-    """Calculates the precision of predictions using to_read data.
+        to_read_df: pd.DataFrame,
+        threshold: float
+) -> Tuple[float, float]:
+    """Calculates the precision and recall of predictions using to_read data.
 
     Args:
         predictions_df (pd.DataFrame): Data frame with predictions.
         to_read_df (pd.DataFrame): Data frame containg to_read data.
+        threshold (float): Treshold for rating to be valid recommendation.
 
     Returns:
-        float: Average precision for all users.
+        Tuple[float, float]: Average `(precision, recall)` for all users.
     """
     def evaluate(group):
         to_read_ids = group['book_id'].values
-        recommended_ids = predictions_df[predictions_df['user_id']
-                                         == group.name]['book_id'].values
-        return precision(recommended_ids, to_read_ids)
+        predictions = predictions_df[predictions_df['user_id'] ==
+                                     group.name][['book_id', 'est']]
+        pred_tuples = [(x.book_id, x.est)
+                       for x in predictions.itertuples()]
 
-    return to_read_df.groupby('user_id').apply(evaluate).mean()
+        return (precision_thresholded(pred_tuples, to_read_ids, threshold),
+                recall_thresholded(pred_tuples, to_read_ids, threshold))
+
+    metrics_series = to_read_df.groupby('user_id').apply(evaluate)
+    df = pd.DataFrame(metrics_series.values.tolist(),
+                      index=metrics_series.index)
+    return (df[df.columns[0]].mean(), df[df.columns[1]].mean())
 
 
 @click.command()
 @click.argument('predictions_dir', type=click.Path(exists=True))
 @click.argument('to_read_filepath', type=click.Path(exists=True))
+@click.option('--threshold', default=4.0,
+              help='Treshold for rating to be valid recommendation.')
 @click.argument('output_filepath', type=click.Path())
-def main(predictions_dir: str, to_read_filepath: str, output_filepath: str):
+def main(predictions_dir: str, to_read_filepath: str,
+         threshold: float, output_filepath: str):
     logger = logging.getLogger(__name__)
 
     predictions_files = listdir(predictions_dir)
@@ -45,11 +58,12 @@ def main(predictions_dir: str, to_read_filepath: str, output_filepath: str):
     results = list()
     for prediction_file in predictions_files:
         prediction_df = pd.read_csv(join(predictions_dir, prediction_file))
-        precision = evaluate_to_read(prediction_df, to_read_df)
-        results.append((prediction_file, precision))
+        precision, recall = evaluate_to_read(
+            prediction_df, to_read_df, threshold)
+        results.append((prediction_file, precision, recall))
 
     logger.info('Saving results to %s...', output_filepath)
-    labels = ['model', 'precision']
+    labels = ['model', 'precision', 'recall']
     results_df = pd.DataFrame.from_records(results, columns=labels)
     results_df.to_csv(output_filepath, index=False)
 
--- tests/evaluation_test.py
+++ tests/evaluation_test.py
@@ -9,10 +9,11 @@ test_case_dir = join(dirname(realpath(__file__)), 'data')
 
 @pytest.mark.parametrize("predictions_filepath, to_read_filepath, expected", [
     (join(test_case_dir, "predictions-simple.csv"),
-     join(test_case_dir, "to_read-simple.csv"), 0.5)
+     join(test_case_dir, "to_read-simple.csv"),
+     ((1 + 0.5 + 0.5)/3, (1 + 0.5 + 0.25)/3))
 ])
 def test_to_read_evaluation(predictions_filepath, to_read_filepath, expected):
     predictions_df = pd.read_csv(predictions_filepath)
     to_read_df = pd.read_csv(to_read_filepath)
-    result = evaluate_to_read(predictions_df, to_read_df)
+    result = evaluate_to_read(predictions_df, to_read_df, 4)
     assert result == expected
